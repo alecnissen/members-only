9a8768a102b17e6c7b0a31d1ea607405d2faa68a
alecnissen <alecnissen@yahoo.com>
2024-07-16 14:04:02 -0400

    used conditionals to determine if user input matches the desired passcode, saving work before using passport so I can gain access to the newly created user and I can update its membership property if user enters correct passcode

--- controllers/joinClub.js
+++ controllers/joinClub.js
@@ -1,5 +1,6 @@
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
+const passport = require("passport");
 
 const User = require("../models/users");
 
@@ -10,3 +11,55 @@ const User = require("../models/users");
 exports.join_club_get = (req, res, next) => { 
     res.render('joinClub', { title: 'Become a member'});
 } 
+
+exports.join_club_post = (req, res, next) => { 
+
+    body('memberPassword', "password must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .toLowerCase()
+
+    const desiredPassword = 'calculator';
+    const userInput = req.body.memberPassword;
+
+    console.log('user input entered', userInput);
+
+    // console.log(User.firstName);
+
+    // how do I access the user that just got created ? 
+    // how do I pass the data from the user that just signed up to the join the club page?
+
+
+
+    try { 
+
+
+
+
+
+
+
+        // if the user input (input field in view) 
+        // matches the desired password var 
+        // display msg
+        // update membership status 
+
+        // how do I get userinput from the view 
+
+        if (userInput === desiredPassword) { 
+            console.log('CORRECT, YOU ARE GRANTED MEMBERSHIP!')
+        } else { 
+            console.log('WRONG ANSWER')
+        }
+
+        
+
+
+
+    } catch (err) { 
+        console.log(err);
+    }
+
+
+}
--- routes/join_club.js
+++ routes/join_club.js
@@ -5,5 +5,6 @@ const joinClubController = require('../controllers/joinClub');
 
 router.get('/', joinClubController.join_club_get);
 
+router.post('/', joinClubController.join_club_post);
 
 module.exports = router;
