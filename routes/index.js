bb33a555d7700364857b68ef5d44ba97bcf649b9
alecnissen <alecnissen@yahoo.com>
2024-07-19 22:30:29 -0400

    added logic so messages are displayed on homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const Message = require('../models/messages');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',  user: req.user });
+router.get('/', async function(req, res, next) {
+
+  try { 
+    const messages = await Message.find();
+    res.render('index', { title: 'Express', user: req.user, messages: messages });
+  } catch (err) { 
+    next(err);
+  }
 });
 
 module.exports = router;
